3f89a9c16033e238569e13c19c34cf0c4534cf7f
Colin Bottrill <cmbottrill@gmail.com>
2026-01-23 13:16:47 -0500

    Create math_utils.h

---
+++ include/math_utils.h
@@ -0,0 +1,16 @@
+#ifndef MATH_UTILS_H
+#define MATH_UTILS_H
+#include <iostream>
+#include <cmath>
+
+
+
+ double sigmoid(double z);
+ double deriv(double a);
+ double rand_uniform(double lo, double hi);
+ double clamp(double x, double lo, double hi);
+
+
+
+
+#endif // MATH_UTILS_H
